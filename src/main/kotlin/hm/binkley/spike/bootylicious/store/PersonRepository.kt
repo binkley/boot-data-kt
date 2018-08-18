d8495a0c6d58333cc18b25375e2d7fbeaea3bd26
Brian Oxley <boxley@thoughtworks.com>
2018-08-18 08:54:45 -0500

    Fix data endpoint

--- src/main/kotlin/hm/binkley/spike/bootylicious/store/PersonRepository.kt
+++ src/main/kotlin/hm/binkley/spike/bootylicious/store/PersonRepository.kt
@@ -1,5 +1,7 @@
 package hm.binkley.spike.bootylicious.store
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
+@RepositoryRestResource(path = "people")
 interface PersonRepository : JpaRepository<PersonRecord, Long>
