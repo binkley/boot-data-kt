729e5ec13073afe83f965e1f531aa4805d9c83ff
Brian Oxley <boxley@thoughtworks.com>
2018-08-27 08:14:50 -0500

    First controller test works

--- src/main/kotlin/hm/binkley/spike/bootylicious/rest/PeopleController.kt
+++ src/main/kotlin/hm/binkley/spike/bootylicious/rest/PeopleController.kt
@@ -5,15 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus.CREATED
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
-@RestController("/api/people")
-class PersonController(
+@RestController
+@RequestMapping("/api/people")
+class PeopleController(
         @Autowired val personFactory: PersonFactory
 ) {
-    @PostMapping("/register")
+    @PostMapping("register")
     @ResponseStatus(CREATED)
     fun register(@Valid @RequestBody request: PersonRegisterRequest) {
         request.toDomain(personFactory).register()
